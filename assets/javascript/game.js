331210f3dbfa7e7f029e3b9541b2335baa84a6c9
Robert Foster <robfos98@gmail.com>
2020-01-29 23:10:05 -0600

    html and css

---
+++ assets/javascript/game.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyBSork4La_XCHyu4MKc4GYbDX3eu9_VpJI",
+    authDomain: "pcojert-36540.firebaseapp.com",
+    databaseURL: "https://pcojert-36540.firebaseio.com",
+    projectId: "pcojert-36540",
+    storageBucket: "pcojert-36540.appspot.com",
+    messagingSenderId: "576973917757",
+    appId: "1:576973917757:web:b2d7f4498fce5071626454"
+};
+
+firebase.initializeApp(firebaseConfig);
+var database = firebase.database();
